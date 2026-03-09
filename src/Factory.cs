81dee85a88dfcb4c57f2f2f75e1f212b26a67485
jade-istaken <jade.dergevorkian@gmail.com>
2026-03-09 15:59:40 -0700

    Added interface for database objects

---
+++ src/Factory.cs
@@ -0,0 +1,12 @@
+using System;
+namespace InventoryManagementSystem
+{
+    internal interface IDatabaseObject
+    {
+        public void AddToDB();
+        //this interface defines the contracts that all objects which eventually get stored in the main database must uphold
+        //namely that you can call the associated method AddToDB() on them to write the information in their object instances to the database
+        //this will be used for Users, Products, and anything else which eventually needs to be written to the DB
+        //extensibility win!
+    }
+}
