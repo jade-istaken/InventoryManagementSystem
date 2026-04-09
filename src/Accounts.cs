8853526d3f107f361ab325dd3c131105afa115cc
jade-istaken <jade.dergevorkian@gmail.com>
2026-04-08 20:15:44 -0700

    refactored accoundling to better utilize EFCore

--- src/Accounts.cs
+++ src/Accounts.cs
@@ -1,41 +1,64 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 namespace InventoryManagementSystem
 {
-    internal abstract class User
-    {
-        public string Username {get;}
-        public Privilege PrivilegeLevel {get; set;}
-        public string FirstName {get;}
-        public string LastName {get;}
+    public enum UserRole {Admin, Staff}
 
-        protected User(string firstName, string lastName)
+    //This is strategy pattern i think? where like the actual password hasher is just an interface, not something concrete 
+    public interface IPasswordHasher
+    {
+        string Hash(string plainText);
+        bool Verify(string hashed, string plainText);
+    }
+
+    //this service is gonna act as a facade for having to interact with the EFCore part
+    public interface IUserService
+    {
+        User CreateUser(string userName, string firstName, string lastName, string plainPass, UserRole role);
+        Task<User?> GetUserAsync(string UserName);
+        Task<bool> ValidateCredialsAsync(string userName, string plainPass);
+    }
+
+    public class UserService : IUserService{
+        private readonly InventoryDbContext _context;
+        private readonly IPasswordHasher _hasher;
+
+        public UserService(InventoryDbContext context, IPasswordHasher hasher)
         {
-            FirstName = firstName;
-            LastName = lastName;
-            Username = FirstName[0] + LastName;
+            _context = context;
+            _hasher = hasher;
         }
-    }
 
-    internal class Administrator : User
-    {
-        public Administrator(string firstName, string lastName) : base (firstName, lastName)
-        {   
-            PrivilegeLevel = Privilege.Administrator;
-        }
-    }
-
-    internal class Staff : User
-    {
-        public Staff(string firstName, string lastName) : base(firstName, lastName)
+        //this is kinda a factory because like. we have the product is just something that implements the User abstract class
+        //and then the actual instantiation is branched
+        public User CreateUser(string userName, string firstName, string lastName, string plainPass, UserRole role)
         {
-            PrivilegeLevel = Privilege.Staff;
-        }
-    }
+            User user = role switch{
+                UserRole.Admin => new Admin(),
+                UserRole.Staff => new Staff(),
+                _ => throw new ArgumentException("Rnvalid role: ", nameof(role))
+            };
 
-    enum Privilege
-    {
-        Administrator,
-        Staff,
-        Guest
+            user.UserName = userName;
+            user.HashedPass = _hasher.Hash(plainPass);
+            user.FirstName = firstName;
+            user.LastName = lastName;
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return user;
+        }
+
+        public async Task<User?> GetUserAsync(string UserName)
+        {
+            return await _context.Users.FindAsync(UserName);
+        }
+
+        public async Task<bool> ValidateCredialsAsync(string userName, string plainPass)
+        {
+            var user = await GetUserAsync(userName);
+            return user != null && _hasher.Verify(user.HashedPass, plainPass);
+        }
     }
 }
